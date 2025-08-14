a1e787f88904ac0b22a7dce5034b824130886a0d
Bhupesh Rawat <154693000+bhupesh45@users.noreply.github.com>
2025-08-14 11:23:17 +0530

    Experiment 2 complete

---
+++ Expetiment 2/EXP2.sql
@@ -0,0 +1,69 @@
+
+
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    EmpName VARCHAR(50) NOT NULL,
+    Department VARCHAR(50) NOT NULL,
+    ManagerID INT NULL  
+);
+
+
+ALTER TABLE Employee
+ADD CONSTRAINT FK_Manager FOREIGN KEY (ManagerID) REFERENCES Employee(EmpID);
+
+
+
+INSERT INTO Employee (EmpID, EmpName, Department, ManagerID)
+VALUES
+(1, 'Alice', 'HR', NULL),       
+(2, 'Bob', 'Finance', 1),
+(3, 'Charlie', 'IT', 1),
+(4, 'David', 'Finance', 2),
+(5, 'Eve', 'IT', 3),
+(6, 'Frank', 'HR', 1);
+
+SELECT E1.EmpName AS[Employee Name],E1.Department AS[EMployeeDept],E2.EmpName AS [ManagerName],E2.Department AS[MangaerDept]
+FROM Employee AS E1
+LEFT JOIN
+Employee AS E2
+ON
+E1.ManagerID = E2.EmpID
+
+CREATE TABLE YEAR_TABLE(
+ID INT,
+YEAR INT,
+NPV INT
+);
+INSERT INTO YEAR_TABLE(ID,YEAR,NPV)
+VALUES
+(1,2018,100),
+(7,2020,30),
+(13,2019,40),
+(1,2019,13),
+(2,2008,121),
+(3,2009,12),
+(11,2020,99),
+(7,2019,0);
+
+CREATE TABLE QUERIES_TABLE(
+ID INT,
+YEAR INT
+);
+INSERT INTO QUERIES_TABLE( ID,YEAR)
+VALUES
+(1,2019),
+(2,2008),
+(3,2009),
+(7,2018),
+(7,2019),
+(7,2020),
+(13,2019);
+
+SELECT Q.ID,Q.YEAR,ISNULL(Y.NPV,0) AS[NPV]
+FROM QUERIES_TABLE AS Q
+LEFT OUTER JOIN
+YEAR_TABLE AS Y
+ON 
+Q.ID = Y.ID
+AND
+Y.YEAR = Q.YEAR
